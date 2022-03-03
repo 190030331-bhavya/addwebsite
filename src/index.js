e49e502632c449b797ee6c4ef35196c36569bf22
190030331-bhavya <bhavyagayathrichitta@gmail.com>
2022-03-03 16:39:40 +0530

    Succesful!

--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Parent extends React.Component{
+  render()
+  {
+    //return <div style={mystyle}>
+    return <div className='header'>
+      <h1>KL UNIVERSITY</h1>
+      <h1>Dept Of Computer Science</h1>
+      <h1>Login Form</h1>
+      </div>
+    
+  }
+
+}
+class Child1 extends React.Component{
+  render(){
+//return <div style={mystyle1}>
+return <div className='header'>
+USER NAME:<br/><input type='text'/><br/>
+<br/>
+PASSWORD:<br/><input type='password'/><br/>
+<br/>
+<button>Submit</button>
+</div>
+  }
+}
+class Child2 extends React.Component{
+  render(){
+return <div className='header1'>
+<p>Copyright@.......</p>
+<li>contact us</li>
+<li>feedback</li>
+</div>
+  }
+}
+class Child extends React.Component{
+  render(){
+
+return <div className='header2'>
+  <Parent/>
+<Child1/>
+<Child2/>
+<ul>footer</ul>
+<ul>Unordered list</ul>
+</div>
+  }
+}
+ReactDOM.render(<Child/>,document.getElementById('root'));
+
